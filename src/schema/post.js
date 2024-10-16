fcddd66c0c7358c8160ce12960bfa9480ef0f92a
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2024-10-16 23:44:58 +0530

    added user and post Schema

---
+++ src/schema/post.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema({
+    caption: {
+      type: String,
+      required: true,
+      minLength: 5,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      
+    },
+  },{ timestamps: true });
+
+const post = mongoose.model("Post", postSchema);
+
+export default post;
