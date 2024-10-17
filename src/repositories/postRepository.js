f39087c24f42274b63f5f5587167dcef0e60d396
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2024-10-17 19:08:28 +0530

    added repository layer

---
+++ src/repositories/postRepository.js
@@ -0,0 +1,37 @@
+import Post from "../schema/post.js";
+
+export const createPost = async (caption, image, user) => {
+  try {
+    const newPost = await Post.create({ caption, image, user });
+    return newPost;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const findAllPosts = async () => {
+  try {
+    const posts = await Post.find();
+    return posts;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const findPostById = async (id) => {
+  try {
+    const post = Post.findById(id);
+    return post;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deletePostById = async (id)=>{
+    try {
+        const post = await Post.findByIdAndDelete(id);
+        return post
+    } catch (error) {
+        console.log(error);
+    }
+}
